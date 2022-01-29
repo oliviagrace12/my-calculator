5112273c51e0111d13be4867b069453bc3f05c3c
Olivia Rojas <olivia.chisman@gmail.com>
2022-01-29 15:51:30 -0600

    initial app working

--- My Calculator/ViewController.swift
+++ My Calculator/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
+    var priorValue: Int = 0
+    var currentValue: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if let value = sender.currentTitle {
+            if (value == "=") {
+                priorValue = priorValue + Int(currentValue)!
+                result.text = "\(priorValue)"
+                priorValue = 0
+                currentValue = ""
+            } else if value == "+" {
+                priorValue = priorValue + (Int(currentValue) != nil ? Int(currentValue)! : 0)
+                currentValue = ""
+                result.text = "\(value)"
+            } else {
+                currentValue = "\(currentValue)\(value)"
+                result.text = "\(currentValue)"
+            }
+        }
+    }
+    
 }
 
